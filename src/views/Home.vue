ce17da538d0c29fc572c8c0fe2b972bcb504faed
Sumair Hassan <sumairhassan10@gmail.com>
2021-02-02 09:42:52 +0100

    Router concept in vue
    Router concept in vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Homepage</h1>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt magnam eligendi quasi consequuntur nisi vitae, ea animi adipisci asperiores culpa similique sapiente ut, temporibus placeat mollitia. Ipsa nemo laudantium veniam voluptatibus et, illum possimus atque at soluta quis repellendus dolorem placeat, consequatur cum magnam, ipsam ut eveniet iure! Doloribus non nobis placeat! Tenetur aut iure voluptatum tempora culpa facilis corporis vel iste quis quae laborum quia veniam, voluptates accusamus suscipit officiis temporibus excepturi beatae animi iusto sapiente error? Tenetur molestiae labore, reprehenderit placeat delectus mollitia qui aperiam est iste suscipit ullam corrupti cum adipisci magni perferendis laudantium ut vel deleniti.</p>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt magnam eligendi quasi consequuntur nisi vitae, ea animi adipisci asperiores culpa similique sapiente ut, temporibus placeat mollitia. Ipsa nemo laudantium veniam voluptatibus et, illum possimus atque at soluta quis repellendus dolorem placeat, consequatur cum magnam, ipsam ut eveniet iure! Doloribus non nobis placeat! Tenetur aut iure voluptatum tempora culpa facilis corporis vel iste quis quae laborum quia veniam, voluptates accusamus suscipit officiis temporibus excepturi beatae animi iusto sapiente error? Tenetur molestiae labore, reprehenderit placeat delectus mollitia qui aperiam est iste suscipit ullam corrupti cum adipisci magni perferendis laudantium ut vel deleniti.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Job Details Page</h1>
+  <p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    // use props or data function, both are same in this case.
+    //data(){
+     //   return{
+       //     id: this.$route.params.id
+        //}
+    // }
+
+}
+</script>
+
+<style>
+
+</style>
